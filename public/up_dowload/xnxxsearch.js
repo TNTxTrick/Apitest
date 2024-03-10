4448c3e96d246a44907372c6e6526adf615fad09
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-10 17:12:37 +0700

    Update xnxxsearch.js

--- public/up_dowload/xnxxsearch.js
+++ public/up_dowload/xnxxsearch.js
@@ -1,17 +1,41 @@
-exports.name = '/xnxxsearch';
-exports.index = async(req, res, next) => {
-const keyword = req.query.keyword;
-if (!keyword) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
 const axios = require('axios');
 
-const options = {
-  method: 'POST',
-  url: 'https://all-media-downloader1.p.rapidapi.com/xnxx_search',
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
-    'X-RapidAPI-Host': 'all-media-downloader1.p.rapidapi.com'
-  },
-  data: {queryXNXX: keyword}
-};
+exports.name = '/xnxxsearch';
+exports.index = async (req, res, next) => {
+    const keyword = req.query.keyword;
+    if (!keyword) {
+        return res.status(400).json({ error: 'Thiếu dữ liệu để khởi chạy chương trình' });
+    }
 
+    const options = {
+        method: 'POST',
+        url: 'https://all-media-downloader1.p.rapidapi.com/xnxx_search',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
+            'X-RapidAPI-Host': 'all-media-downloader1.p.rapidapi.com'
+        },
+        data: { queryXNXX: keyword }
+    };
+
+    try {
+        const response = await axios.request(options);
+        if (response.status === 200) {
+            return res.json(response.data);
+        } else {
+            return res.status(response.status).json({ error: 'Có lỗi xảy ra khi tải xuống dữ liệu từ API' });
+        }
+    } catch (error) {
+        console.error(error);
+        if (error.response) {
+            
+            return res.status(error.response.status).json({ error: 'Có lỗi xảy ra khi tải xuống dữ liệu từ API' });
+        } else if (error.request) {
+            
+            return res.status(500).json({ error: 'Có lỗi xảy ra khi gửi yêu cầu đến server' });
+        } else {
+          
+            return res.status(500).json({ error: 'Có lỗi xảy ra trong quá trình xử lý yêu cầu' });
+        }
+    }
+};
