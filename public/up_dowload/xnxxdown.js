f3de06882c35b4dc9d38c48c7fa43e2bb1eb870f
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-10 17:14:01 +0700

    Create xnxxdown.js

---
+++ public/up_dowload/xnxxdown.js
@@ -0,0 +1,16 @@
+
+const axios = require('axios');
+
+const options = {
+  method: 'POST',
+  url: 'https://all-media-downloader1.p.rapidapi.com/xnxx',
+  headers: {
+    'content-type': 'application/json',
+    'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
+    'X-RapidAPI-Host': 'all-media-downloader1.p.rapidapi.com'
+  },
+  data: {
+    urlXNXX: link
+  }
+};
+
