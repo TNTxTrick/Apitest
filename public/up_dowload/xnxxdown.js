385f73270a6e6828330a51e6be69eba87b8ab233
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-10 17:16:27 +0700

    Update xnxxdown.js

--- public/up_dowload/xnxxdown.js
+++ public/up_dowload/xnxxdown.js
@@ -1,25 +1,44 @@
-
 const axios = require('axios');
 
-const options = {
-  method: 'POST',
-  url: 'https://all-media-downloader1.p.rapidapi.com/xnxx',
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
-    'X-RapidAPI-Host': 'all-media-downloader1.p.rapidapi.com'
-  },
-  data: {
-    urlXNXX: link
-  }
+exports.name = '/xnxxdown';
+exports.index = async (req, res, next) => {
+    const link = req.query.link;
+    if (!link) {
+        return res.status(400).json({ error: 'Thiếu dữ liệu để khởi chạy chương trình.' });
+    }
+
+    const options = {
+        method: 'POST',
+        url: 'https://all-media-downloader1.p.rapidapi.com/xnxx',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
+            'X-RapidAPI-Host': 'all-media-downloader1.p.rapidapi.com'
+        },
+        data: {
+            urlXNXX: link
+        }
+    };
+
+    try {
+        const response = await axios.request(options);
+        if (response.status === 200) {
+            return res.json(response.data);
+        } else {
+            return res.status(response.status).json({ error: 'Có lỗi xảy ra khi tải xuống dữ liệu từ API' });
+        }
+    } catch (error) {
+        console.error(error);
+        if (error.response) {
+          
+            return res.status(error.response.status).json({ error: 'Có lỗi xảy ra khi tải xuống dữ liệu từ API' });
+        } else if (error.request) {
+           
+            return res.status(500).json({ error: 'Có lỗi xảy ra khi gửi yêu cầu đến server' });
+        } else {
+          
+            return res.status(500).json({ error: 'Có lỗi xảy ra trong quá trình xử lý yêu cầu' });
+        }
+    }
 };
 
-try {
-    const response = await axios.request(options);
-    console.log(response.data);
-    return res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    return res.json({ error: 'Có lỗi xảy ra khi tìm kiếm trên API.' });
-  }
-};
