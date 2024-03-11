2d378e51d7a8bc2d88ad4597d97ff2b729cc1f25
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-11 19:17:49 +0700

    Update fbb.js

--- public/up_dowload/fbb.js
+++ public/up_dowload/fbb.js
@@ -1,24 +1,28 @@
-
 const axios = require('axios');
 
-const options = {
-  method: 'GET',
-  url: 'https://facebook-video-audio-download.p.rapidapi.com/geturl',
-  params: {
-    video_url: 'https://www.facebook.com/reel/523477522932448'
-  },
-  headers: {
-    'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
-    'X-RapidAPI-Host': 'facebook-video-audio-download.p.rapidapi.com'
+exports.name = '/fb';
+exports.index = async (req, res, next) => {
+  const link = req.query.link;
+  if (!link) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình' });
+
+  const options = {
+    method: 'GET',
+    url: 'https://facebook-video-audio-download.p.rapidapi.com/geturl',
+    params: {
+      video_url: link
+    },
+    headers: {
+      'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
+      'X-RapidAPI-Host': 'facebook-video-audio-download.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const response = await axios.request(options);
+    return res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    return res.json({ error: 'Có lỗi xảy ra khi tải xuống dữ liệu từ Facebook' });
   }
 };
 
-try {
-      const response = await axios.request(options);
-      console.log(response.data);
-      return res.json(response.data);
-    } catch (error) {
-      console.error(error);
-      return res.json({ error: 'Có lỗi xảy ra khi tải xuống dữ liệu từ Youtube' });
-    }
-  };
