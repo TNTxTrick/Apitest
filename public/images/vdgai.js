eb4d8157783e6d91a22e7c5b3308d41a110a28bc
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-14 12:51:05 +0700

    Create vdgai.js

---
+++ public/images/vdgai.js
@@ -0,0 +1,14 @@
+exports.name = '/images/vdgai';
+exports.index = async(req, res, next) => {
+    try {
+        const gai = require('./data/json/gai.json');
+        var image = gai[Math.floor(Math.random() * gai.length)].trim();
+        res.jsonp({
+            url: image,
+            count: gai.length,
+            author: 'Tnt'
+        });
+    } catch (e) {
+        return res.jsonp({ error: e });
+    }
+}
