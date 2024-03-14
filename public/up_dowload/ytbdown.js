34786f6045b65465c5fb6a6cde7a68053c0e225c
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-14 21:52:00 +0700

    Update ytbdown.js

--- public/up_dowload/ytbdown.js
+++ public/up_dowload/ytbdown.js
@@ -1,3 +1,9 @@
+exports.name = '/ytbdown';
+exports.index = async (req, res, next) => {
+  const link = req.query.link;
+  if (!link) {
+    return res.status(400).json({ error: 'Thiếu đường dẫn Instagram để tải xuống' });
+  }
 
 const axios = require('axios');
 
@@ -13,4 +19,11 @@ const options = {
   }
 };
 
-
+try {
+    const response = await axios.request(options);
+    return res.json(response.data);
+  } catch (error) {
+    console.error('Lỗi khi tải từ API:', error);
+    return res.status(500).json({ error: 'Có lỗi xảy ra khi tải từ API' });
+  }
+};
