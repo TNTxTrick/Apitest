babe9f2027d01b4308c36da4abd307909170ac2b
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-14 21:50:51 +0700

    Update ytbdown.js

--- public/up_dowload/ytbdown.js
+++ public/up_dowload/ytbdown.js
@@ -1,1 +1,16 @@
 
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://youtube-audio-video-download.p.rapidapi.com/geturl',
+  params: {
+    video_url: link
+  },
+  headers: {
+    'X-RapidAPI-Key': 'c677a2e9e3msh9944fd67c431ac3p1e4ca3jsn693a78eab94d',
+    'X-RapidAPI-Host': 'youtube-audio-video-download.p.rapidapi.com'
+  }
+};
+
+
