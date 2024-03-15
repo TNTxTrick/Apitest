bf048b65328687a86d7ef95a26e0ef257ab5f2a8
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-15 16:21:37 +0700

    Create random.js

---
+++ public/up_dowload/random.js
@@ -0,0 +1,82 @@
+exports.name = '/random';
+exports.index = async (req, res, next) => {
+  const axios = require('axios');
+  const fs = require('fs'); 
+
+  let {apikey} = req.query;
+  
+  if (!apikey || apikey !== 'randomtnt'){
+    return res.json({result: 'Lấy apikey liên hệ admin'})
+  }
+   
+  const path = [
+    "https://vt.tiktok.com/ZSF5w7AwF/",
+    "https://vt.tiktok.com/ZSFAwd12G/",
+    "https://vt.tiktok.com/ZSFATEP5u/",
+    "https://vt.tiktok.com/ZSFAw8HGx/", 
+    "https://vt.tiktok.com/ZSFATWcUT/", 
+    "https://vt.tiktok.com/ZSFAweTN7/", 
+    "https://vt.tiktok.com/ZSFAw1qhJ/", 
+    "https://vt.tiktok.com/ZSFAwBRND/", 
+    "https://vt.tiktok.com/ZSFAwmQMp/",
+    "https://vt.tiktok.com/ZSFAKRBpk/", 
+    "https://vt.tiktok.com/ZSFAKFGH1/", 
+    "https://vt.tiktok.com/ZSFAKfhhx/",
+    "https://vt.tiktok.com/ZSFAKU8D1/",
+    "https://vt.tiktok.com/ZSFAKeTya/", 
+    "https://vt.tiktok.com/ZSFAKMXtX/", 
+    "https://vt.tiktok.com/ZSFAKBEun/", 
+    "https://vt.tiktok.com/ZSFAKjveP/", 
+    "https://vt.tiktok.com/ZSFAKY1kw/", 
+    "https://vt.tiktok.com/ZSFAK25LB/", 
+    "https://vt.tiktok.com/ZSFAKBY3v/", 
+    "https://vt.tiktok.com/ZSFDnNvqX/", 
+    "https://vt.tiktok.com/ZSFDtoLLa/", 
+    "https://vt.tiktok.com/ZSFDndVbD/", 
+    "https://vt.tiktok.com/ZSFDnN7Ys/", 
+    "https://vt.tiktok.com/ZSFDn5moV/", 
+    "https://vt.tiktok.com/ZSFDnrpgM/", 
+    "https://vt.tiktok.com/ZSFDnmsxq/", 
+    "https://vt.tiktok.com/ZSFDn9Ywa/", 
+    "https://vt.tiktok.com/ZSFDnhbG3/",
+    "https://vt.tiktok.com/ZSFDnuypD/", 
+    "https://vt.tiktok.com/ZSFDnfnKU/",
+    "https://vt.tiktok.com/ZSFDnSxvP/", 
+    "https://vt.tiktok.com/ZSFDna5jq/", 
+    "https://vt.tiktok.com/ZSFDnVHdm/",
+    "https://vt.tiktok.com/ZSF5yFBGA/", 
+    "https://vt.tiktok.com/ZSF5yXHmj/", 
+    "https://vt.tiktok.com/ZSF5y5K6D/",
+    "https://vt.tiktok.com/ZSF5yAAk4/", 
+    "https://vt.tiktok.com/ZSF5yHfF3/", 
+    "https://vt.tiktok.com/ZSF5ygMhr/", 
+    "https://vt.tiktok.com/ZSF5ymLRr/", 
+    "https://vt.tiktok.com/ZSF5yXtn1/", 
+    "https://vt.tiktok.com/ZSF5w7AwF/"
+
+  ];
+
+  const randomCodm = path[Math.floor(Math.random() * path.length)];
+
+  try {
+    const response = await axios.post('https://www.tikwm.com/api/', { url: randomCodm }, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } });
+    const videoUrl = response.data.data.play;
+    const userName = response.data.data.author.unique_id;
+    const usernickname = response.data.data.author.nickname;
+    const title = response.data.data.title;
+    const id = response.data.data.id;
+    const likes = response.data.data.digg_count;
+    const comments = response.data.data.comment_count;
+    const share = response.data.data.share_count;
+    const views = response.data.data.play_count;
+    const video = response.data.data.video_url;
+
+    res.send(response.data);
+
+    console.log({VideoUrl: videoUrl, Username: userName, Usernickname: usernickname, Title: title, ID: id, Likes: likes, Comments: comments, Share: share, Views: views});
+	
+  } catch (error) {
+    console.error(error);
+    return res.json({result: error.message});
+  }
+};
