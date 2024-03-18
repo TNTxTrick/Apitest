63092e4148e5282fff017f6a17c24ac876575b28
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-18 09:31:58 +0700

    Update linkytb.js

--- public/link/linkytb.js
+++ public/link/linkytb.js
@@ -1,7 +1,7 @@
 exports.name = '/link/linkytb';
 exports.index = async(req, res, next) => {
     try {
-        const linkytb = require('./data/json/link.json');
+        const linkytb = require('./data/json/linkytb.json');
         var image = linkytb[Math.floor(Math.random() * linkytb.length)].trim();
         res.jsonp({
             url: image,
