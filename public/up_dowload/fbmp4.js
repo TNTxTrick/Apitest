17965396fdbaedc02dff52bc737fc46b958cb550
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-19 17:42:20 +0700

    Update fbmp4.js

--- public/up_dowload/fbmp4.js
+++ public/up_dowload/fbmp4.js
@@ -1,10 +1,15 @@
+exports.name = '/fbmp4';
+exports.index = async (req, res, next) => {
+  const link = req.query.link;
+  if (!link) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
+
 const axios = require('axios');
 
 const options = {
   method: 'GET',
   url: 'https://facebook-video-and-reel-downloader.p.rapidapi.com/',
   params: {
-    url: 'https://www.facebook.com/share/v/LRDiKKduGnbM2TRt/?mibextid=xfxF2i'
+    url: link
   },
   headers: {
     'X-RapidAPI-Key': 'c677a2e9e3msh9944fd67c431ac3p1e4ca3jsn693a78eab94d',
@@ -12,3 +17,12 @@ const options = {
   }
 };
 
+try {
+    const response = await axios.request(options);
+    console.log(response.data);
+    return res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    return res.json({ error: 'Có lỗi xảy ra khi tải xuống dữ liệu từ API' });
+  }
+};
