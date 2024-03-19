39ffca8baa3825355f262f0912c648d3bb505e23
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-19 22:12:51 +0700

    Update snapchat.js

--- public/up_dowload/snapchat.js
+++ public/up_dowload/snapchat.js
@@ -1,11 +1,14 @@
-
+exports.name = '/snapchat';
+exports.index = async(req, res, next) => {
+const link = req.query.link;
+if (!link) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
 const axios = require('axios');
 
 const options = {
   method: 'GET',
   url: 'https://download-snapchat-video-spotlight-online.p.rapidapi.com/download',
   params: {
-    url: 'https://t.snapchat.com/xnS0NlBb '
+    url: link
   },
   headers: {
     'X-RapidAPI-Key': 'c677a2e9e3msh9944fd67c431ac3p1e4ca3jsn693a78eab94d',
@@ -13,3 +16,12 @@ const options = {
   }
 };
 
+try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      return res.json(response.data);
+    } catch (error) {
+      console.error(error);
+      return res.json({ error: 'Có lỗi xảy ra khi tải từ API' });
+    }
+  };
