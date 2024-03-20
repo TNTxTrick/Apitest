800e049954ed8ccb3d903f99c83dcea929834992
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-20 18:28:16 +0700

    Update tikmusic.js

--- public/up_dowload/tikmusic.js
+++ public/up_dowload/tikmusic.js
@@ -1,3 +1,7 @@
+exports.name = '/tikmusic';
+exports.index = async (req, res, next) => {
+  const link = req.query.link;
+  if (!link) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
 
 const axios = require('axios');
 
@@ -13,3 +17,12 @@ const options = {
   }
 };
 
+try {
+    const response = await axios.request(options);
+    console.log(response.data);
+    return res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    return res.json({ error: 'Có lỗi xảy ra khi tải xuống dữ liệu từ Pinterest' });
+  }
+};
