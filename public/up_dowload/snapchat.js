d5dac71c0b93cb76f61ede2ad9ff0d4a0f09e468
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-20 08:46:37 +0700

    Update snapchat.js

--- public/up_dowload/snapchat.js
+++ public/up_dowload/snapchat.js
@@ -1,3 +1,6 @@
+const keyAPi = ['d0ab76bc06msh5032ca2f6f3baf9p15f9d8jsn024d834a55cb','c677a2e9e3msh9944fd67c431ac3p1e4ca3jsn693a78eab94d'];
+const getRandomKey = () => keyAPi[Math.floor(Math.random() * keyAPi.length)];
+
 exports.name = '/snapchat';
 exports.index = async(req, res, next) => {
 const link = req.query.link;
@@ -11,7 +14,7 @@ const options = {
     url: link
   },
   headers: {
-    'X-RapidAPI-Key': 'c677a2e9e3msh9944fd67c431ac3p1e4ca3jsn693a78eab94d',
+    'X-RapidAPI-Key': getRandomKey()
     'X-RapidAPI-Host': 'download-snapchat-video-spotlight-online.p.rapidapi.com'
   }
 };
