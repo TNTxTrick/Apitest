7e2022842fdf0d1bca9484f56ce7741aaa6894b0
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-21 19:27:57 +0700

    Update fbmp4.js

--- public/up_dowload/fbmp4.js
+++ public/up_dowload/fbmp4.js
@@ -1,4 +1,4 @@
-const keyAPi = ['94208fec7fmsh64b5be7c2bdd72dp17ce8cjsna3e04d56dbc7','d0ab76bc06msh5032ca2f6f3baf9p15f9d8jsn024d834a55cb','b619707d57mshc1e2f8dec3870ecp12e04cjsnb42dfa0c28ca','c677a2e9e3msh9944fd67c431ac3p1e4ca3jsn693a78eab94d'];
+const keyAPi = ['94208fec7fmsh64b5be7c2bdd72dp17ce8cjsna3e04d56dbc7','d0ab76bc06msh5032ca2f6f3baf9p15f9d8jsn024d834a55cb','b619707d57mshc1e2f8dec3870ecp12e04cjsnb42dfa0c28ca','c677a2e9e3msh9944fd67c431ac3p1e4ca3jsn693a78eab94d','02ad1e368emsh6ab614c75885f35p173664jsn692af787e902'];
 const getRandomKey = () => keyAPi[Math.floor(Math.random() * keyAPi.length)];
 
 exports.name = '/fbmp4';
