e089c54a4816908856dedd3e1683ffeebe2a7031
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-21 19:14:37 +0700

    Update fbmp4.js

--- public/up_dowload/fbmp4.js
+++ public/up_dowload/fbmp4.js
@@ -1,3 +1,6 @@
+const keyAPi = ['fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d','d0ab76bc06msh5032ca2f6f3baf9p15f9d8jsn024d834a55cb','b619707d57mshc1e2f8dec3870ecp12e04cjsnb42dfa0c28ca','c677a2e9e3msh9944fd67c431ac3p1e4ca3jsn693a78eab94d'];
+const getRandomKey = () => keyAPi[Math.floor(Math.random() * keyAPi.length)];
+
 exports.name = '/fbmp4';
 exports.index = async (req, res, next) => {
   const link = req.query.link;
@@ -12,7 +15,7 @@ const options = {
     url: link
   },
   headers: {
-    'X-RapidAPI-Key': 'c677a2e9e3msh9944fd67c431ac3p1e4ca3jsn693a78eab94d',
+    'X-RapidAPI-Key': getRandomKey(),
     'X-RapidAPI-Host': 'facebook-video-and-reel-downloader.p.rapidapi.com'
   }
 };
