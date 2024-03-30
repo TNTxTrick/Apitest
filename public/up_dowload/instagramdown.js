777c78fd6b0133a97c44cae3cad38727073c3d0b
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-30 12:45:41 +0700

    Update instagramdown.js

--- public/up_dowload/instagramdown.js
+++ public/up_dowload/instagramdown.js
@@ -11,7 +11,7 @@ const options = {
     url: link
   },
   headers: {
-    'X-RapidAPI-Key': '47a23919a4msh78b95d902d5a830p12dcddjsnf8bcbe88ed59',
+    'X-RapidAPI-Key': '3630a660f7msh5928760034ca557p1a6373jsnf2ebaa2cc40e',
     'X-RapidAPI-Host': 'instagram-story-downloader-media-downloader.p.rapidapi.com'
   }
 };
