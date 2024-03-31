b95724c2246be6994798a5c48aafb588c8c5afe3
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-31 15:31:58 +0700

    Update igstory.js

--- public/up_dowload/igstory.js
+++ public/up_dowload/igstory.js
@@ -1,3 +1,7 @@
+exports.name = '/igstory';
+exports.index = async (req, res, next) => {
+  const link = req.query.link;
+  if (!link) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
 
 const axios = require('axios');
 
@@ -5,7 +9,7 @@ const options = {
   method: 'GET',
   url: 'https://instagram-post-reels-stories-downloader.p.rapidapi.com/instagram/',
   params: {
-    url: 'https://www.instagram.com/stories/minhthu04._/3335117007806563854?igsh=cWo0c2xscmV3b21l'
+    url: link
   },
   headers: {
     'X-RapidAPI-Key': '3630a660f7msh5928760034ca557p1a6373jsnf2ebaa2cc40e',
@@ -13,3 +17,12 @@ const options = {
   }
 };
 
+try {
+    const response = await axios.request(options);
+    console.log(response.data);
+    return res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    return res.json({ error: 'Có lỗi xảy ra khi tải xuống dữ liệu từ Pinterest' });
+  }
+};
