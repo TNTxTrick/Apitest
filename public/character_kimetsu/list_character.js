f24efb5c246be0462dd1cf74f119bbeb6549ada2
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-04-03 20:58:03 +0700

    Update list_character.js

--- public/character_kimetsu/list_character.js
+++ public/character_kimetsu/list_character.js
@@ -12,7 +12,7 @@ exports.index = async (req, res, next) => {
                 const name = $(element).find("a").attr("title");
                 const url = $(element).find("a").attr("href");
                 const image = $(element).find("a > img").attr("data-src");
-                const cleanUrl = "https://api-7izq.onrender.com" + url.substring(url.indexOf("/wiki"));
+                const cleanUrl = "https://api-7izq.onrender.com" + url.substring(url.indexOf("/info"));
                 thumbnails.push({ name, url: cleanUrl, image });
             });
             if (limit && limit > 0) {
