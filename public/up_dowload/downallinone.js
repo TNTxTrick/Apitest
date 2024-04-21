c76091fc9ef7a17ded698af6f03cdd6529b34bf6
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-04-21 12:34:37 +0700

    Update downallinone.js

--- public/up_dowload/downallinone.js
+++ public/up_dowload/downallinone.js
@@ -10,7 +10,7 @@ exports.index = async (req, res, next) => {
     url: 'https://social-download-all-in-one.p.rapidapi.com/v1/social/autolink',
     headers: {
       'content-type': 'application/json',
-      'X-RapidAPI-Key': '5cb46a06bbmshcb43e68340c7c0ap12cfc8jsne9b4b7272ac0',
+      'X-RapidAPI-Key': 'b1b134d34cmsh9196ad10efa01eap12279cjsn01b3c727f546',
       'X-RapidAPI-Host': 'social-download-all-in-one.p.rapidapi.com'
     },
     data: {
