c3d9c29a049c86774918cc8ff94edd9b3e20ab42
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-04-23 15:12:08 +0700

    Update dovui.js

--- public/game/dovui.js
+++ public/game/dovui.js
@@ -1,20 +1,9 @@
-const fs = require('fs');
-
 exports.name = '/game/dovui';
-exports.index = async (req, res, next) => {
-    try {
-        const resp = JSON.parse(fs.readFileSync("./data/dovui.json"));
-        const length = resp.length;
-        const randomIndex = Math.floor(Math.random() * length);
-        const randomQuestion = resp[randomIndex];
-        
-        res.setHeader('Content-Type', 'application/json');
-        return res.json({ 
-            author: 'tnt',
-            data: randomQuestion
-        });
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'Internal Server Error' });
-    }
-};
+exports.index = async(req, res, next) => {
+const resp = require("./data/dovui.json");
+const length = resp.length
+return res.json({ 
+	author: 'tnt',
+	data: resp[Math.floor(Math.random() * length)]
+ })
+}
