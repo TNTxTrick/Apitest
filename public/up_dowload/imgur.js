4ed2ff7985c28cf26c2889ed31c1cd04c63713d4
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-04-25 20:37:22 +0700

    Update imgur.js

--- public/up_dowload/imgur.js
+++ public/up_dowload/imgur.js
@@ -1,25 +1,23 @@
-const fs = require("fs");
-const request = require('request');
-
 exports.name = '/imgur';
 exports.index = async(req, res, next) => {
+    var request = require('request');
     var link = req.query.link;
     if (!link) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
-    const { path, type } = await dl(link);
+    var request = require('request');
     var options = {
         'method': 'POST',
-        'url': 'https://api.imgur.com/3/upload',
+        'url': 'https://api.imgur.com/3/image',
         'headers': {
             'Authorization': 'Client-ID c76eb7edd1459f3'
+        },
+        formData: {
+            'image': link
         }
     };
-
-    options.formData = type == "video" ? {'video': fs.createReadStream(path)} : {'image': fs.createReadStream(path)}
     request(options, function(error, response) {
         if (error) return res.json({ error: 'Đã xảy ra lỗi với link của bạn' })
-        var linkk = response.body
-        var upload = JSON.parse(linkk)
-        fs.unlinkSync(path);
+        var data = response.body
+        var upload = JSON.parse(data)
         res.json({
             uploaded: {
                 status: 'success',
@@ -27,20 +25,4 @@ exports.index = async(req, res, next) => {
             }
         })
     });
-}
-
-async function dl(url) {
-  return new Promise((rs, rj) => {
-  let path;
-    request(url).on('response', function (response) {
-      const ext = response.headers['content-type'].split('/')[1];
-      
-      path = __dirname + `/cache/${Date.now()}.${ext}`;
-      response
-        .pipe(fs.createWriteStream(path))
-        .on('finish', () => {
-          rs({ path, type: response.headers['content-type'].split('/')[0] })
-        })
-    })
-});
 }
