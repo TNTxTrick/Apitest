4c32be6ede51dd6b4a8ad7e33fcbc33ce3882842
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-04-30 18:37:53 +0700

    Update tintuc.js

--- public/ff/tintuc.js
+++ public/ff/tintuc.js
@@ -1,41 +1,39 @@
-const cheerio = require('cheerio');
 const axios = require('axios');
+const cheerio = require('cheerio');
 
+exports.name = '/ff/news/:id';
 
-
+exports.index = async (req, res, next) => {
   const id = req.params.id; 
-  axios.get('https://ff.garena.com/vn/article/' + id)
-    .then(response => {
-      const html = response.data;
-      const $ = cheerio.load(html);
+  try {
+    const response = await axios.get(`https://ff.garena.com/vn/article/${id}`);
+    const html = response.data;
+    const $ = cheerio.load(html);
 
-      const date = $('span.m-date').text();
-      const title = $('div.banner-mode h1.title').text();
-      const content = $('p span strong').map((_, elem) => $(elem).text().trim()).get();
-      const img = $('span img').attr('src');
-      const image =$('figure img').attr('src');
+    const date = $('span.m-date').text();
+    const title = $('div.banner-mode h1.title').text();
+    const content = $('div.article-content').text().trim(); // Adjust selector to match article content
+    const img = $('span img').attr('src');
+    const figureImg = $('figure img').attr('src');
 
-      console.log('Title:', title);
+    console.log('Title:', title);
 
-      const contentArray = content.map(line => line.trim());
+    const images = [];
+    if (img) images.push(img);
+    if (figureImg) images.push(figureImg);
 
-      const images = [];
-      if (img) images.push(img);
-      if (image) images.push(image);
+    const responseData = {
+      date: date,
+      title: title,
+      content: content,
+      images: images
+    };
 
-      const responseData = {
-        date: date,
-        title: title,
-        content: contentArray,
-        images: images
-      };
-
-      res.json(responseData);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      res.status(500).send('Error fetching data');
-    });
-});
+    res.json(responseData);
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+    res.status(500).json({ error: 'Error fetching data' });
+  }
+};
 
 
