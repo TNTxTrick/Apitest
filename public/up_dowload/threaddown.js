5d57b1b0528899551bd00b8396054a202eafaf96
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-05-09 18:24:54 +0700

    Update threaddown.js

--- public/up_dowload/threaddown.js
+++ public/up_dowload/threaddown.js
@@ -11,7 +11,7 @@ const options = {
     url: link
   },
   headers: {
-    'X-RapidAPI-Key': 'b1b134d34cmsh9196ad10efa01eap12279cjsn01b3c727f546',
+    'X-RapidAPI-Key': '15375c47d5msh3698cbdb105ac9bp167948jsn5ac92ee40cca',
     'X-RapidAPI-Host': 'threads-photo-video-crawler.p.rapidapi.com'
   }
 };
