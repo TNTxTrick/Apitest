408b02c7077d0ff5583612ddc336021594e473b9
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-05-10 14:59:05 +0700

    Update sssthreads.js

--- public/up_dowload/sssthreads.js
+++ public/up_dowload/sssthreads.js
@@ -1,23 +1,21 @@
-const axios = require('axios');
-
-// Define your index function
-exports.index = async (req, res, next) => {
-    var link = req.query.link;
-    if (!link) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
-
-    axios({
-        method: 'post',
-        url: 'https://api.threadsphotodownloader.com/v2/media?url=' + link,
-        data: {    
-            url: link
-        }
-    })
-    .then(function (response) {
-        var data = response.data
-        console.log(data)
-        return res.json(data)
-    })
-    .catch(function (error) {
-        return res.json({ error });
-    });
+exports.name = '/threads';
+exports.index = async(req, res, next) => {
+var link = req.query.link;
+if (!link) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
+var axios = require('axios');
+axios({
+    method: 'post',
+    url: 'https://api.threadsphotodownloader.com/v2/media?url=' + link,
+    data: {    
+	url: link
+	}
+})
+.then(function (response) {
+    var data = response.data
+    console.log(data)
+    return res.json(data)
+})
+.catch(function (error) {
+    return res.json({ error });
+});
 }
