d973261ddc33d5b2ea5879ea79d79818bb0ef721
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-05-10 21:26:11 +0700

    Update pinterest.js

--- public/up_dowload/pinterest.js
+++ public/up_dowload/pinterest.js
@@ -1,26 +1,21 @@
-exports.name = '/pinterest/down';
-exports.index = async (req, res, next) => {
-  const link = req.query.link;
-  if (!link) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
-
-  const axios = require('axios');
-
-  const options = {
-    method: 'GET',
-    url: 'https://pinterest-downloader-download-pinterest-image-video-and-reels.p.rapidapi.com/api/getdata',
-    params: { url: link },
-    headers: {
-      'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
-      'X-RapidAPI-Host': 'pinterest-downloader-download-pinterest-image-video-and-reels.p.rapidapi.com'
-    }
-  };
-
-  try {
-    const response = await axios.request(options);
-    console.log(response.data);
-    return res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    return res.json({ error: 'Có lỗi xảy ra khi tải xuống dữ liệu từ Pinterest' });
-  }
-};
+exports.name = '/pinterestdown';
+exports.index = async(req, res, next) => {
+var link = req.query.link;
+if (!link) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
+var axios = require('axios');
+axios({
+    method: 'post',
+    url: 'https://pinterestdownloader.io/vi/frontendService/DownloaderService?url=' + link,
+    data: {    
+	url: link
+	}
+})
+.then(function (response) {
+    var data = response.data
+    console.log(data)
+    return res.json(data)
+})
+.catch(function (error) {
+    return res.json({ error });
+});
+}
