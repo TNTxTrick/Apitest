aa6bbdd90f83563516a10795574c61c6bcf3d420
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-05-14 13:22:18 +0700

    Update doutube.js

--- public/up_dowload/doutube.js
+++ public/up_dowload/doutube.js
@@ -1,23 +1,21 @@
-exports.name = '/doutube/info';
-exports.index = async (req, res, next) => {
-    var id = req.query.id;
-    if (!id) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình' });
-
-    try {
-        var axios = require('axios');
-        var url = `https://api.doutu.be/api/video/?author=${id}&skips=0&limit=1`;
-        console.log(`Requesting URL: ${url}`);
-        var response = await axios.post(url, { url: id });
-        var data = response.data;
-        console.log(data);
-        return res.json(data);
-    } catch (error) {
-        console.error(`Error making request: ${error.message}`);
-        if (error.response) {
-            console.error(`Response data: ${JSON.stringify(error.response.data)}`);
-            console.error(`Response status: ${error.response.status}`);
-            console.error(`Response headers: ${JSON.stringify(error.response.headers)}`);
-        }
-        return res.json({ error: error.message });
-    }
-};
+exports.name = '/doutube';
+exports.index = async(req, res, next) => {
+var link = req.query.link;
+if (!link) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
+var axios = require('axios');
+axios({
+    method: 'post',
+    url: 'https://api.doutu.be/api/video/?author=' + link + '&skips=0&limit=1',
+    data: {    
+	url: link
+	}
+})
+.then(function (response) {
+    var data = response.data
+    console.log(data)
+    return res.json(data)
+})
+.catch(function (error) {
+    return res.json({ error });
+});
+}
