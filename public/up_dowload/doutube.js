4312329038162484d3e2752c394650169400e591
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-05-14 13:08:50 +0700

    Update doutube.js

--- public/up_dowload/doutube.js
+++ public/up_dowload/doutube.js
@@ -1,21 +1,15 @@
 exports.name = '/doutube/info';
-exports.index = async(req, res, next) => {
-var id = req.query.id;
-if (!id) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
-var axios = require('axios');
-axios({
-    method: 'post',
-    url: 'https://api.doutu.be/api/video/?author=' + id + '&skips=0&limit=1',
-    data: {    
-	url: id
-	}
-})
-.then(function (response) {
-    var data = response.data
-    console.log(data)
-    return res.json(data)
-})
-.catch(function (error) {
-    return res.json({ error });
-});
-}
+exports.index = async (req, res, next) => {
+    var id = req.query.id;
+    if (!id) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình' });
+
+    try {
+        var axios = require('axios');
+        var response = await axios.post(`https://api.doutu.be/api/video/?author=${id}&skips=0&limit=1`, { url: id });
+        var data = response.data;
+        console.log(data);
+        return res.json(data);
+    } catch (error) {
+        return res.json({ error: error.message });
+    }
+};
