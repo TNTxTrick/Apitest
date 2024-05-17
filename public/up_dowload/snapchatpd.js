f2e937efd0d5517902a27ca22395cdd571ef53e4
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-05-17 18:55:45 +0700

    Create snapchatpd.js

---
+++ public/up_dowload/snapchatpd.js
@@ -0,0 +1,23 @@
+const url = req.query.url;
+  if (!url) {
+    return res.status(400).send('Please provide a URL as a query parameter');
+  }
+
+  try {
+    const { data } = await axios.get(`https://dlpanda.com/vi/snapchat?url=${encodeURIComponent(url)}`);
+    const $ = cheerio.load(data);
+    let videoSrc = [];
+
+    
+    $('video source').each((index, element) => {
+      const src = $(element).attr('src');
+      if (src) {
+        videoSrc.push(src); // Thêm đường dẫn ảnh vào mảng
+      }
+    });
+
+    res.json({ videoSrc:videoSrc });
+  } catch (error) {
+    res.status(500).send('Error occurred while scraping');
+  }
+});
