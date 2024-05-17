1f76d1a0081470b1ee07cb638df13781a5a44cd8
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-05-17 16:31:30 +0700

    Create tiktokpanda.js

---
+++ public/up_dowload/tiktokpanda.js
@@ -0,0 +1,21 @@
+const url = req.query.url;
+  if (!url) {
+    return res.status(400).send('Please provide a URL as a query parameter');
+  }
+
+  try {
+    const { data } = await axios.get(`https://dlpanda.com/vi?url=${encodeURIComponent(url)}&token=G7eRpMaa`);
+    const $ = cheerio.load(data);
+    let scrapedData = [];
+
+    // Scraping the video source URL
+    $('video source').each((index, element) => {
+      const videoUrl = $(element).attr('src');
+      scrapedData.push({ videoUrl });
+    });
+
+    res.json(scrapedData);
+  } catch (error) {
+    res.status(500).send('Error occurred while scraping');
+  }
+});
