d905d7263ed0f98eb78415682085b9bede39f009
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-05-17 19:01:00 +0700

    Update snapchatpd.js

--- public/up_dowload/snapchatpd.js
+++ public/up_dowload/snapchatpd.js
@@ -1,3 +1,8 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+exports.name = '/snapchatpd';
+exports.index = async (req, res, next) => {
 const url = req.query.url;
   if (!url) {
     return res.status(400).send('Please provide a URL as a query parameter');
@@ -12,7 +17,7 @@ const url = req.query.url;
     $('video source').each((index, element) => {
       const src = $(element).attr('src');
       if (src) {
-        videoSrc.push(src); // Thêm đường dẫn ảnh vào mảng
+        videoSrc.push(src); 
       }
     });
 
