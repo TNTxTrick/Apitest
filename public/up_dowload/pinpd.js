b05af9db21f91d4418cf2f4045f21341b3f43948
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-05-17 19:46:39 +0700

    Update pinpd.js

--- public/up_dowload/pinpd.js
+++ public/up_dowload/pinpd.js
@@ -1,23 +1,29 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+exports.name = '/pinpd';
+
 const url = req.query.url;
-  if (!url) {
-    return res.status(400).send('Please provide a URL as a query parameter');
-  }
 
-  try {
-    const { data } = await axios.get(`https://dlpanda.com/vi/pinterest?url=${encodeURIComponent(url)}`);
+if (!url) {
+  return res.status(400).send('Please provide a URL as a query parameter');
+}
 
-    const $ = cheerio.load(data);
-    let imgSrc = [];
+try {
+  const { data } = await axios.get(`https://dlpanda.com/vi/pinterest?url=${encodeURIComponent(url)}`);
 
-    $('.col-md-12.col-lg-6 img').each((index, element) => {
-      const src = $(element).attr('src');
-      if (src) {
-        imgSrc.push(src); // Add image URL to the array
-      }
-    });
+  const $ = cheerio.load(data);
+  let imgSrc = [];
 
-    res.json({ imgSrc: imgSrc });
-  } catch (error) {
-    res.status(500).send('Error occurred while scraping');
-  }
-});
+  $('.col-md-12.col-lg-6 img').each((index, element) => {
+    const src = $(element).attr('src');
+    if (src) {
+      imgSrc.push(src); 
+    }
+  });
+
+  res.json({ imgSrc: imgSrc });
+} catch (error) {
+  res.status(500).send('Error occurred while scraping');
+}
+
