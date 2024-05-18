7156c0667a6655a1c963a34d0aac6037437468d4
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-05-18 10:15:26 +0700

    Update douyin.js

--- public/up_dowload/douyin.js
+++ public/up_dowload/douyin.js
@@ -1,1 +1,12 @@
+const axios = require('axios');
 
+exports.name = '/douyin';
+exports.index = async (req, res, next) => {
+  const link = req.query.link; 
+  try {
+    const response = await axios.get('https://godownloader.com/api/tiktok-no-watermark-free?key=godownloader.com&url=' + link);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send(error.toString());
+  }
+};
