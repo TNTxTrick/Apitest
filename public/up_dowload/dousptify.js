6e558a9cdd1579e9520d7e679cea25a466d9bc0a
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-05-20 13:15:15 +0700

    Update dousptify.js

--- public/up_dowload/dousptify.js
+++ public/up_dowload/dousptify.js
@@ -4,8 +4,17 @@ exports.name = '/download/track';
 exports.index = async (req, res, next) => {
   const link = req.query.link;
   try {
-    const response = await axios.get(`https://spotifyapi.caliphdev.com/api/download/track?url=${link}`);
-    res.json(response.data);
+    const response = await axios({
+      url: `https://spotifyapi.caliphdev.com/api/download/track?url=${link}`,
+      method: 'GET',
+      responseType: 'stream'  // Ensure response is in stream format
+    });
+
+    res.setHeader('Content-Disposition', 'attachment; filename=track.mp3');
+    res.setHeader('Content-Type', 'audio/mpeg');
+
+    response.data.pipe(res);  // Pipe the stream directly to the response
+
   } catch (error) {
     res.status(500).send(error.toString());
   }
