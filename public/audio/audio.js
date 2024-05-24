7f4a8151af63057dcdba015996781eee1a8c6a2b
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-05-24 21:16:05 +0700

    Update audio.js

--- public/audio/audio.js
+++ public/audio/audio.js
@@ -1,1 +1,14 @@
-
+exports.name = '/audio';
+exports.index = async(req, res, next) => {
+    try {
+        const audio = require('./data/audio.json');
+        var audio = audio[Math.floor(Math.random() * audio.length)].trim();
+        res.jsonp({
+            url: audio,
+            count: audio.length,
+            author: 'Tnt'
+        });
+    } catch (e) {
+        return res.jsonp({ error: e });
+    }
+}
