ab717de0b6b2fa0849d560b1cf5105079bc109dc
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-01 12:34:26 +0700

    Update character.js

--- public/gsi/character.js
+++ public/gsi/character.js
@@ -1,1 +1,12 @@
+const axios = require("axios")
 
+exports.name = '/gsi/character/:id';
+exports.index = async (req, res, next) => {
+  const { id } = req.params; // Extract the id parameter
+  try {
+    const response = await axios.get(`https://gsi.fly.dev/characters/${id}`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send(error.toString());
+  }
+};
