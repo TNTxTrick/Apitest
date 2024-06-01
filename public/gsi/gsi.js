956c77a8d007e4af034ddea7b090a035dfd00a81
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-01 12:31:30 +0700

    Update gsi.js

--- public/gsi/gsi.js
+++ public/gsi/gsi.js
@@ -1,1 +1,11 @@
+const axios = require('axios');
 
+exports.name = '/gsi';
+exports.index = async (req, res, next) => {
+  try {
+    const response = await axios.get(`https://gsi.fly.dev/`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send(error.toString());
+  }
+};
