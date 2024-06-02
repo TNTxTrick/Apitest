92f45a1f81cb008991e8db6a3a89e285aac94ab0
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-02 09:43:48 +0700

    Update infotruyen.js

--- public/truyentranh/infotruyen.js
+++ public/truyentranh/infotruyen.js
@@ -3,18 +3,21 @@ const axios = require("axios");
 exports.name = '/truyen/:keyword';
 
 exports.index = async (req, res, next) => {
-const keyword = req.params.keyword; // Access route parameter using req.params
+  const keyword = req.params.keyword; // Access route parameter using req.params
 
   try {
     const response = await axios.get(`https://otruyenapi.com/v1/api/truyen-tranh/${keyword}`);
 
-    const descriptionHead = response.data.data.seoOnPage.descriptionHead;
-    const image = response.data.data.seoOnPage.seoSchema.image;    
-    const items = response.data.data.item;
+    const { descriptionHead, seoSchema, item } = response.data.data;
 
-    res.json({ descriptionHead: descriptionHead, image: image, items: items});
+    res.json({ descriptionHead, image: seoSchema.image, items: item });
   } catch (error) {
     console.error(error); // Log any errors
-    res.status(500).send('Lỗi khi lấy data');
+
+    if (error.response && error.response.status === 404) {
+      res.status(404).send('Không tìm thấy truyện'); // Resource not found
+    } else {
+      res.status(500).send('Lỗi khi lấy dữ liệu');
+    }
   }
-});
+};
