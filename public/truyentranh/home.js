253d3acbc026c3c527f45755e291c665763f9ca2
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-02 08:03:58 +0700

    Create home.js

---
+++ public/truyentranh/home.js
@@ -0,0 +1,23 @@
+const axios = reqiure("axios")
+
+try {
+    const response = await axios.get('https://otruyenapi.com/v1/api/home');
+    console.log(response.data); // Log the entire response to inspect its structure
+    const params = response.data.data.params
+    const items = response.data.data.items.map(item => ({
+      _id: item._id,
+      name: item.name,
+      slug: item.slug,
+      origin_name: item.origin_name,
+      status: item.status,
+      thumb_url: 'https://img.otruyenapi.com/uploads/comics/' + item.thumb_url,
+      updatedAt: item.updatedAt,
+      chaptersLatest: item.chaptersLatest,
+    }));
+      
+    res.json({ items: items, params: params });
+  } catch (error) {
+    console.error(error); // Log any errors
+    res.status(500).send('Error fetching data from API');
+  }
+});
