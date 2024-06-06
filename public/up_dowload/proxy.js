bc65a784514e6894f5d7943a27ce2633de1b4dfb
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-06 13:03:15 +0700

    Create proxy.js

---
+++ public/up_dowload/proxy.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+exports.name = '/proxy';
+exports.index = async (req, res) => {
+  try {
+    const response = await axios.get('https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&proxy_format=ipport&format=text&timeout=20000');
+    const data = response.data;
+
+    res.set('Content-Type', 'text/plain');
+    res.send(data);
+  } catch (error) {
+    console.error('Error fetching proxy data:', error);
+    res.status(500).send('An error occurred while fetching proxy data');
+  }
+};
