6c95b86512334c5fdb3596cdb3861897961c5935
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-06 12:23:36 +0700

    Update proxy.js

--- public/proxy/proxy.js
+++ public/proxy/proxy.js
@@ -3,22 +3,13 @@ const axios = require('axios');
 exports.name = '/proxy';
 exports.index = async (req, res) => {
   try {
-    const response = await axios.get('https://api.proxyscrape.com/v3/free-proxy-list/get', {
-      params: {
-        request: 'displayproxies',
-        proxy_format: 'ipport',
-        format: 'text',
-        timeout: 9211
-      }
-    });
+    const response = await axios.get('https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&proxy_format=ipport&format=text&timeout=20000');
+    const data = response.data;
 
-    // Trả về dữ liệu API mà không chỉnh sửa
-    res.send(response.data);
+    res.set('Content-Type', 'text/plain');
+    res.send(data);
   } catch (error) {
-    // Log lỗi để debug
-    console.error('Error fetching data:', error.message);
-
-    // Trả về thông báo lỗi cho client
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error('Error fetching proxy data:', error);
+    res.status(500).send('An error occurred while fetching proxy data');
   }
 };
