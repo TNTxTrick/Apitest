1f5e5944096eeebf9be65482fbcd1c7fbd7dee20
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-12 10:01:26 +0700

    Update character.js

--- public/honkai/character.js
+++ public/honkai/character.js
@@ -1,7 +1,6 @@
+exports.name = '/honkai/character/:name';
 const axios = require('axios');
 const cheerio = require('cheerio');
-
-exports.name = '/honkai/character/:name';
 
 exports.index = async (req, res, next) => {
 const name = req.params.name;
