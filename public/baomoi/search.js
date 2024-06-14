363bfd877e2ebb2cef4202de47488c1373c27ea7
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-14 17:17:17 +0700

    Update search.js

--- public/baomoi/search.js
+++ public/baomoi/search.js
@@ -1,1 +1,18 @@
+const axios = require("axios");
 
+exports.name = '/baomoi/search';
+
+exports.index = async (req, res, next) => {
+  const keyword = req.query.keyword;
+
+  try {
+    const response = await axios.get(`https://w-api.baomoi.com/api/v1/content/get/list-by-custom?listType=search&keyword=${keyword}&page=2&ctime=1718359846&version=0.6.52&sig=6a76cfc7e92855425393f369656b856100b0b9e0aff6cd5e6db6573af66859c2&apiKey=kI44ARvPwaqL7v0KuDSM0rGORtdY1nnw`);
+
+    const items = response.data.data.items;
+    
+    res.json({ items: items });
+  } catch (error) {
+    console.error(error); // Log any errors
+    res.status(500).send('Lỗi khi lấy data');
+  }
+};
