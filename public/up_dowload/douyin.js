80acb145a9fa8e188b2497e424f2e567e836e0d1
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-14 10:26:14 +0700

    Create douyin.js

---
+++ public/up_dowload/douyin.js
@@ -0,0 +1,16 @@
+
+const axios = require('axios');
+
+const options = {
+  method: 'POST',
+  url: 'https://douyin-media-no-watermark1.p.rapidapi.com/v1/social/douyin/web/aweme/detailurl',
+  headers: {
+    'x-rapidapi-key': '15375c47d5msh3698cbdb105ac9bp167948jsn5ac92ee40cca',
+    'x-rapidapi-host': 'douyin-media-no-watermark1.p.rapidapi.com',
+    'Content-Type': 'application/json'
+  },
+  data: {
+    url: link
+  }
+};
+
