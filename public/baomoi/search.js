636a6f04a351dd2aab104f18084cc1dbd85570a0
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-15 11:23:44 +0700

    Update search.js

--- public/baomoi/search.js
+++ public/baomoi/search.js
@@ -5,14 +5,31 @@ exports.name = '/baomoi/search';
 exports.index = async (req, res, next) => {
   const keyword = req.query.keyword;
 
+  if (!keyword) {
+    return res.status(400).send('Keyword is required');
+  }
+
+  const link = `https://w-api.baomoi.com/api/v1/content/get/list-by-custom?listType=search&keyword=${keyword}&page=2&ctime=1718359846&version=0.6.52&sig=6a76cfc7e92855425393f369656b856100b0b9e0aff6cd5e6db6573af66859c2&apiKey=kI44ARvPwaqL7v0KuDSM0rGORtdY1nnw`;
+
   try {
-    const response = await axios.get(`https://w-api.baomoi.com/api/v1/content/get/list-by-custom?listType=search&keyword=${keyword}&page=2&ctime=1718359846&version=0.6.52&sig=6a76cfc7e92855425393f369656b856100b0b9e0aff6cd5e6db6573af66859c2&apiKey=kI44ARvPwaqL7v0KuDSM0rGORtdY1nnw`);
+    const response = await axios.get(link);
+    const data = response.data; // Entire response data
 
-    const data = response.data.data.items;
+    if (data.err !== 0) {
+      throw new Error(data.msg || 'Error retrieving data');
+    }
 
-    res.json(data);
+    const items = data.data.items; // Extract the items array
+    const ids = items.map(item => ({ id: item.id })); // Extract and wrap each id in {}
+
+    res.json({ ids }); // Send the ids in the response
   } catch (error) {
-    console.error(error); // Log any errors
-    res.status(500).send('Lỗi khi lấy data');
+    console.error('Error fetching data from BaoMoi API:', error); // Log any errors
+
+    if (error.response) {
+      res.status(error.response.status).send('Không tìm thấy data');
+    } else {
+      res.status(500).send('Lỗi khi lấy dữ liệu: ' + error.message); // Send detailed error message
+    }
   }
 };
