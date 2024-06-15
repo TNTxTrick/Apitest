4ba8830d516b2583eda9e5938b42e0dc9e404a59
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-15 10:41:33 +0700

    Update search.js

--- public/baomoi/search.js
+++ public/baomoi/search.js
@@ -9,19 +9,13 @@ exports.index = async (req, res, next) => {
     return res.status(400).send('Keyword is required');
   }
 
-  // Default to page 1 if not provided
-
   const link = `https://w-api.baomoi.com/api/v1/content/get/list-by-custom?listType=search&keyword=${keyword}&page=2&ctime=1718359846&version=0.6.52&sig=6a76cfc7e92855425393f369656b856100b0b9e0aff6cd5e6db6573af66859c2&apiKey=kI44ARvPwaqL7v0KuDSM0rGORtdY1nnw`;
 
   try {
     const response = await axios.get(link);
-    const items = response.data?.data?.items;
+    const data = response.data.data;
 
-    if (!items || items.length === 0) {
-      return res.status(404).send('Không tìm thấy data');
-    }
-
-    res.json({ items });
+    res.json(data);
   } catch (error) {
     console.error('Error fetching data from BaoMoi API:', error); // Log any errors
 
