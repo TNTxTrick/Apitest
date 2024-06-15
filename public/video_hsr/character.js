bfc480e19e8b8781b258a58d7311a590e3604d6c
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-15 13:33:05 +0700

    Update and rename video.js to character.js

--- public/video_hsr/character.js
+++ public/video_hsr/character.js
@@ -1,4 +1,4 @@
-exports.name = '/video_hsr';
+exports.name = '/character/honkai3';
 exports.index = async(req, res, next) => {
   const data = require('./data/data.json');
   var id = req.query.id;
@@ -8,9 +8,21 @@ exports.index = async(req, res, next) => {
   var ID = info.ID
   var name = info.name;
   var image = info.image;
+  var story = info.story;
+  var skill = info.skill;
+  var weapon = info.weapon;
+  var ring = info.ring;
+  var type = info.type;
+  var type_dmg = info.type_dmg;
   return res.json({
     ID,
     name,
+    story,
+    skill,
+    weapon,
+    ring,
+    type,
+    type_dmg,
     image
   })
 } 
