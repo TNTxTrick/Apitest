611fb9a6c2c83e30ad02f266d0ef5e6775d27e43
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-15 11:08:22 +0700

    Update search.js

--- public/baomoi/search.js
+++ public/baomoi/search.js
@@ -8,7 +8,7 @@ exports.index = async (req, res, next) => {
   try {
     const response = await axios.get(`https://w-api.baomoi.com/api/v1/content/get/list-by-custom?listType=search&keyword=${keyword}&page=2&ctime=1718359846&version=0.6.52&sig=6a76cfc7e92855425393f369656b856100b0b9e0aff6cd5e6db6573af66859c2&apiKey=kI44ARvPwaqL7v0KuDSM0rGORtdY1nnw`);
 
-    const data = response.data.data;
+    const data = response.data.data.items;
 
     res.json(data);
   } catch (error) {
