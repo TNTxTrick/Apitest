7186ac7023689eb1a69c189828f83967d4ec1224
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-16 10:55:15 +0700

    Create home.js

---
+++ public/truyen18+/home.js
@@ -0,0 +1,35 @@
+try {
+    const url = 'https://damconuong.net';
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+
+    const items = [];
+
+    $('div.relative').each((index, element) => {
+      const img = $(element).find('img.rounded-t-lg.cover').attr('src');
+      const title = $(element).find('a.text-white').text().trim();
+      const link = $(element).find('a.text-white').attr('href');
+      const chapnew = $(element).find('div.p-2 a').text().trim(); 
+      const link_chapnew = $(element).find('div.p-2 a ').attr('href');
+      const update = $(element).find('span.text-gray-400.text-xs').text().trim();
+
+      const found = items.some(item => item.title === title);
+
+      if (!found) {
+        items.push({
+          title: title,
+          img: img,
+          link: 'https://damconuong.net' + link,
+          chapnew: chapnew,
+          link_chapnew: 'https://damconuong.net' + link_chapnew,
+          update: update
+        });
+      }
+    });
+
+    return items;
+  } catch (error) {
+    console.error('Error occurred while scraping:', error);
+    throw error;
+  }
+};
