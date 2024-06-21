2c8f23184fb48d1ef7314a0c34ec293fb44bd047
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-21 14:47:22 +0700

    Update proxy.js

--- public/proxy/proxy.js
+++ public/proxy/proxy.js
@@ -7,7 +7,6 @@ try {
     const response = await axios.get('https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&proxy_format=ipport&format=text&timeout=20000');
     const data = response.data;
 
-    res.set('Content-Type', 'text/plain');
     res.send(data);
   } catch (error) {
     console.error('Error fetching proxy data:', error);
