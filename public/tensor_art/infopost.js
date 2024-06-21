c9617cde1612eb3c666441625302de14ba31f63e
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-21 21:12:18 +0700

    Update infopost.js

--- public/tensor_art/infopost.js
+++ public/tensor_art/infopost.js
@@ -1,3 +1,8 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+exports.name = '/tensor/infopost';
+exports.index = async (req, res, next) => {
 const link = req.query.link;
   if (!link) {
     return res.status(400).send('Link is required');
@@ -22,4 +27,4 @@ const link = req.query.link;
         console.error('Error fetching data from the API:', error);
         res.status(500).send('Error fetching data from the API');
       }
-    });
+    };
