5f1ab3a2c79a2192c9ba1618c441f8cf110f44fd
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-24 17:06:22 +0700

    Create urlscan.js

---
+++ public/up_dowload/urlscan.js
@@ -0,0 +1,13 @@
+const domain = req.query.domain;
+    if (!domain) {
+        return res.status(400).json({ error: 'Domain query parameter is required' });
+    }
+
+    try {
+        const response = await axios.get(`https://urlscan.io/api/v1/search/?q=domain:${domain}`);
+        res.json(response.data);  // Return only the res.data from the API call
+    } catch (error) {
+        console.error('Error fetching data from urlscan.io:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
