36577abe1084dbe362ac7d8d2d71a763ad1a79d2
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-06-25 16:42:42 +0700

    Create image_anime.js

---
+++ public/up_dowload/image_anime.js
@@ -0,0 +1,12 @@
+try {
+    const response = await axios.get('https://pic.re/image', {
+      responseType: 'arraybuffer',
+    });
+    const imageBuffer = Buffer.from(response.data, 'binary');
+    res.set('Content-Type', response.headers['content-type']);
+    res.send(imageBuffer);
+  } catch (error) {
+    console.error('Error fetching image:', error);
+    res.status(500).send('Error fetching image');
+  }
+};
