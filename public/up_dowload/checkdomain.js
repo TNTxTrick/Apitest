a535bba001e272ea15164b2e277567f4673ec193
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-07-01 15:53:29 +0700

    Update checkdomain.js

--- public/up_dowload/checkdomain.js
+++ public/up_dowload/checkdomain.js
@@ -1,4 +1,8 @@
- const domain = req.params.domain;
+const axios = require('axios');
+
+exports.name = '/checkdomain/:domain';
+exports.index = async (req, res, next) => {
+const domain = req.params.domain;
     try {
         const response = await fetch(`https://whois.inet.vn/api/whois/domainspecify/${domain}`);
         const data = await response.json();
