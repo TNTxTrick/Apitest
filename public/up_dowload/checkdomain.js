1ad71d1a6500da71074ef8bd13437a89ede1a79e
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-07-01 15:52:13 +0700

    Create checkdomain.js

---
+++ public/up_dowload/checkdomain.js
@@ -0,0 +1,9 @@
+ const domain = req.params.domain;
+    try {
+        const response = await fetch(`https://whois.inet.vn/api/whois/domainspecify/${domain}`);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
