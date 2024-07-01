c4c60e3e13c3b99c112aa6b93d411141bfdcaf26
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-07-01 16:23:53 +0700

    Update checkdomain.js

--- public/up_dowload/checkdomain.js
+++ public/up_dowload/checkdomain.js
@@ -2,12 +2,12 @@ const axios = require('axios');
 
 exports.name = '/checkdomain';
 exports.index = async (req, res, next) => {
-  const domain = req.query.domain;
-  if (!domain) {
+  const url = req.query.url;
+  if (!url) {
     return res.status(400).json('Vui lòng nhập Domain')
   }
   try {
-    const response = await axios.get(`https://whois.inet.vn/api/whois/domainspecify/${domain}`);
+    const response = await axios.get(`https://whois.inet.vn/api/whois/domainspecify/${url}`);
     res.json(response.data);
   } catch (error) {
     res.status(500).send('Something went wrong!');
