083512daccbbcd6534248e64177d1e3a80a6af8b
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-07-16 14:46:33 +0700

    Update douyin.js

--- public/up_dowload/douyin.js
+++ public/up_dowload/douyin.js
@@ -1,28 +1,34 @@
 exports.name = '/douyin';
 exports.index = async (req, res, next) => {
-  const link = req.query.link;
-  if (!link) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình' }); 
   const axios = require('axios');
 
-const options = {
-  method: 'POST',
-  url: 'https://douyin-media-no-watermark1.p.rapidapi.com/v1/social/douyin/web/aweme/detailurl',
-  headers: {
-    'x-rapidapi-key': '15375c47d5msh3698cbdb105ac9bp167948jsn5ac92ee40cca',
-    'x-rapidapi-host': 'douyin-media-no-watermark1.p.rapidapi.com',
-    'Content-Type': 'application/json'
-  },
-  data: {
-    url: link
+  const url = req.query.url;
+  if (!url) {
+    return res.status(400).send('Thiếu URL');
   }
-};
 
- try {
-    const response = await axios.request(options);
-    console.log(response.data);
-    return res.json(response.data);
+  try {
+    const { data } = await axios.get(`https://dlpanda.com/vi?token=G7eRpMaa&url=${encodeURIComponent(url)}`);
+    const $ = cheerio.load(data);
+    let scrapedData = [];
+    let imgSrc = [];
+
+    $('.col-md-12.col-lg-6 img').each((index, element) => {
+      const src = $(element).attr('src');
+      if (src) {
+        imgSrc.push(src); 
+      }
+    });
+
+    $('video source').each((index, element) => {
+      const videoUrl = $(element).attr('src');
+      if (videoUrl) {
+        scrapedData.push({ url: 'https:' + videoUrl });
+      }
+    });
+
+    res.json({ videos: scrapedData, images: imgSrc });
   } catch (error) {
-    console.error(error);
-    return res.json({ error: 'Có lỗi xảy ra khi tải video từ API' });
+    res.status(500).send('Lỗi');
   }
 };
