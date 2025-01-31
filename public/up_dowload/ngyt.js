76e1b3fbccdc1bad287392c7b982af52d23a0ed4
TNT <139092950+TNTxTrick@users.noreply.github.com>
2025-01-31 19:31:14 +0700

    Update ngyt.js

--- public/up_dowload/ngyt.js
+++ public/up_dowload/ngyt.js
@@ -31,11 +31,18 @@ exports.index = async (req, res) => {
       form,
       {
         headers: {
-          ...form.getHeaders(),
+            ...form.getHeaders(),
           'accept': '*/*',
           'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
+          'content-type': 'multipart/form-data; boundary=----WebKitFormBoundarycM5O8kn0doYfT0Bf',
+          'cookie': '_ga_2VPRRCG6WX=GS1.1.1737986084.1.0.1737986084.0.0.0; _ga=GA1.1.419157133.1737986085; __gads=ID=2891ee68a3aed1bb:T=1737986084:RT=1737986084:S=ALNI_Mb0RhYjq8H5ectS6eRix2PBo3L8Cg; __eoi=ID=0ab2f635eedcd19f:T=1737986084:RT=1737986084:S=AA-AfjbFY15L4GmCgmENhbD3lLng',
           'origin': 'https://nhanguiyeuthuong.me',
+          'priority': 'u=1, i',
           'referer': 'https://nhanguiyeuthuong.me/',
+          'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
           'sec-fetch-mode': 'cors',
           'sec-fetch-site': 'same-origin',
           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
