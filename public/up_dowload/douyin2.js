8f42151449c0aef4c614d71714dbe5f23ca9d985
TNT <139092950+TNTxTrick@users.noreply.github.com>
2025-02-01 16:14:08 +0700

    Update douyin2.js

--- public/up_dowload/douyin2.js
+++ public/up_dowload/douyin2.js
@@ -18,10 +18,19 @@ exports.index = async (req, res, next) => {
             }),
             {
                 headers: {
-                    'accept': '*/*',
-                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-                    'origin': 'https://snapvideo.io',
-                    'referer': 'https://snapvideo.io/tai-video-douyin-khong-logo/'
+          'accept': '*/*',
+          'accept-language': 'en-US,en;q=0.9,vi;q=0.8',
+          'cookie': 'PHPSESSID=9fj0t26c34c01mn6knjl7i8nqf; pll_language=vi; _ga_93DGJ7EPCL=GS1.1.1737967731.1.0.1737967731.0.0.0; _ga=GA1.1.2008311444.1737967731; pvc_visits[0]=1738054125b172; prefetchAd_8481177=true',
+          'origin': 'https://snapvideo.io',
+          'priority': 'u=1, i',
+          'referer': 'https://snapvideo.io/tai-video-douyin-khong-logo/',
+          'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-origin',
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
                 }
             }
         );
