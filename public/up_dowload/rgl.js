0d0b3547abd1aeb2825a986503006b1ca79d2076
TNT <139092950+TNTxTrick@users.noreply.github.com>
2025-02-08 15:17:44 +0700

    Update rgl.js

--- public/up_dowload/rgl.js
+++ public/up_dowload/rgl.js
@@ -5,7 +5,7 @@ exports.name = '/rgl/check';
 exports.index = async (req, res, next) => {
   const id = req.query.id; 
   try {
-    const response = await axios.get(`https://rgl-m8d7.onrender.com/rgl/check?id=${id}`);
+    const response = await axios.get(`https://rgl-m8d7.onrender.com/check?id=${id}`);
     res.json(response.data);
   } catch (error) {
     res.status(500).send(error.toString());
