78880dd028124221264e5ce3d066b930557d4704
TNT <139092950+TNTxTrick@users.noreply.github.com>
2025-02-08 14:48:23 +0700

    Create rgl.js

---
+++ public/up_dowload/rgl.js
@@ -0,0 +1,13 @@
+
+const axios = require('axios');
+
+exports.name = '/rgl/check';
+exports.index = async (req, res, next) => {
+  const id = req.query.id; 
+  try {
+    const response = await axios.get(`https://rgl-m8d7.onrender.com/rgl/check=${id}`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send(error.toString());
+  }
+};
